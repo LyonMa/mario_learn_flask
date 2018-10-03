1030577429d10ca264bfd558d040d70e33b73dc6
LyonMa <6777mario@gmail.com>
2018-10-03 14:12:22 +0800

    chapter 8 totally done

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -33,3 +33,35 @@ class RegistrationForm(FlaskForm):
 	def validate_username(self, field):
 		if User.query.filter_by(username=field.data).first():
 			raise ValidationError('Username already in use.')
+
+
+class ChangePasswordForm(FlaskForm):
+	old_password = PasswordField('Old Password', validators=[DataRequired()])
+	password = PasswordField('New Password', validators=[
+		DataRequired(), EqualTo('password2', message='Passwords must match.')])
+	password2 = PasswordField('Confirm Password', validators=[DataRequired()])
+	submit = SubmitField('Update Password')
+
+
+class PasswordResetRequestForm(FlaskForm):
+	email = StringField('Email', validators=[DataRequired(), Length(1, 64),
+											 Email()])
+	submit = SubmitField('Reset Password')
+
+
+class PasswordResetForm(FlaskForm):
+	password = PasswordField('New Password', validators=[DataRequired(),
+				EqualTo('password2', message='Passwords must match.')])
+	password2 = PasswordField('Confirm Password', validators=[DataRequired()])
+	submit = SubmitField('Reset Password')
+
+
+class ChangeEmailForm(FlaskForm):
+	email = StringField('New Email', validators=[DataRequired(), Length(1, 64),
+												 Email()])
+	password = PasswordField('Password', validators=[DataRequired()])
+	submit = SubmitField('Update Email Address')
+
+	def validate_email(self, field):
+		if User.query.filter_by(email=field.data).first():
+			raise ValidationError('Email already registered.')
