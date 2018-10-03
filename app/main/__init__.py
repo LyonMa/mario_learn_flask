9e79c94e298523be829a03bfa7042a1185a3c2f8
LyonMa <6777mario@gmail.com>
2018-10-03 16:35:38 +0800

    chapter 9 totally done

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -4,3 +4,7 @@ main = Blueprint('main', __name__)
 
 # 避免循环导入依赖
 from . import views, errors
+
+@main.app_context_processor
+def inject_permissions():
+	return dict(Permission=Permission)
--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -102,3 +102,37 @@ class UserModelTestCase(unittest.TestCase):
 		token = u2.generate_email_change_token('john@example.com')
 		self.assertFalse(u2.change_email(token))
 		self.assertTrue(u2.email == 'susan@example.org')
+
+	def test_user_role(self):
+		u = User(email='john@example.com', password='cat')
+		self.assertTrue(u.can(Permission.FOLLOW))
+		self.assertTrue(u.can(Permission.COMMENT))
+		self.assertTrue(u.can(Permission.WRITE))
+		self.assertFalse(u.can(Permission.MODERATE))
+		self.assertFalse(u.can(Permission.ADMIN))
+
+	def test_moderator_role(self):
+		r = Role.query.filter_by(name='Moderator').first()
+		u = User(email='john@example.com', password='cat', role=r)
+		self.assertTrue(u.can(Permission.FOLLOW))
+		self.assertTrue(u.can(Permission.COMMENT))
+		self.assertTrue(u.can(Permission.WRITE))
+		self.assertTrue(u.can(Permission.MODERATE))
+		self.assertFalse(u.can(Permission.ADMIN))
+
+	def test_admin_role(self):
+		r = Role.query.filter_by(name='Administrator').first()
+		u = User(email='john@example.com', password='cat', role=r)
+		self.assertTrue(u.can(Permission.FOLLOW))
+		self.assertTrue(u.can(Permission.COMMENT))
+		self.assertTrue(u.can(Permission.WRITE))
+		self.assertTrue(u.can(Permission.MODERATE))
+		self.assertTrue(u.can(Permission.ADMIN))
+
+	def test_anonymous_user(self):
+		u = Anonymous_User()
+		self.assertFalse(u.can(Permission.FOLLOW))
+		self.assertFalse(u.can(Permission.COMMENT))
+		self.assertFalse(u.can(Permission.WRITE))
+		self.assertFalse(u.can(Permission.MODERATE))
+		self.assertFalse(u.can(Permission.ADMIN))
